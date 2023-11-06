8dac36bb2fb167f94356f0185eb58cd2b8eefbf9
ChrisQGeorge <chrisqgeorge@gmail.com>
2023-11-06 15:49:59 -0500

    Added ability to import mint balance history from trends

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def exportBalances(balances):
     
     i = 1
     while fileName in exportFiles:
-        fileName = baseFileName + str(i) + ".csv"
+        fileName = baseFileName + " (" + str(i) + ").csv"
         i = i+1
 
 
@@ -55,40 +55,8 @@ def selectImportFile(importDir):
 
     return selection
 
+def getBalanceFromTransactions(transactions):
 
-def importMintTransactions():
-    pass
-
-
-def importMintBalances():
-    pass
-
-
-def importMonarchTransactions():
-    importDir = getPath("import")
-    selection = selectImportFile(importDir)
-    runningBal = int(float(re.sub(",|\$","",input("What is the current account balance?: ")))*100)
-    transactions = {}
-
-    print(selection)
-    with open(str(selection), newline='') as impFile:
-        unsortedTable = csv.reader(impFile)
-
-        next(unsortedTable)
-
-        table = sorted(unsortedTable, key = lambda row: datetime.strptime(row[0], "%Y-%m-%d"), reverse=True)
-
-        for row in table:
-            if not row[0] == "Date":
-                date = (datetime.strptime(row[0], '%Y-%m-%d') - datetime(1899, 12, 30)).days
-                account = row[3]
-                amount = int(float(row[6])*100)
-                transactions[date] = [date, runningBal, account]
-
-                runningBal -= amount
-
-        transactions[date-1] = [date-1, runningBal, account]
-    
     tally = 0
     i = min(transactions.keys())
     end = max(transactions.keys())
@@ -111,6 +79,80 @@ def importMonarchTransactions():
         balances.append([(datetime(1899, 12, 30) + timedelta(days=int(transactions[date][0]))).strftime('%Y-%m-%d'), transactions[date][1]/100, transactions[date][2]])
         
     return balances
+
+
+def importMintTransactions():
+    importDir = getPath("import")
+    selection = selectImportFile(importDir)
+
+
+
+
+def importMintBalances():
+    importDir = getPath("import")
+    selection = selectImportFile(importDir)
+    accountName = input("What is the Monarch account name?")
+    loadTable = []
+    with open(str(selection), newline='') as impFile:
+        unsortedTable = csv.reader(impFile)
+
+        next(unsortedTable)
+
+        table = sorted(unsortedTable, key = lambda row: datetime.strptime(row[0], "%b-%y"))
+        
+        for row in table:
+            date = (datetime.strptime(row[0], '%b-%y') - datetime(1899, 12, 30)).days
+            balance = row[1].replace("$", '').replace(",", '')
+            loadTable.append([date, balance, accountName])
+
+    balances = [["Date","Balance","Account"]]
+    currDate = loadTable[0][0]
+    i = 0
+    runningBalance = loadTable[0][1]
+    while i <= len(loadTable)-1:
+
+        while currDate <= loadTable[i][0]:
+
+            if currDate == loadTable[i][0]:runningBalance = loadTable[i][1]
+
+            balances.append([((datetime(1899, 12, 30) + timedelta(days=int(currDate))).strftime('%Y-%m-%d')), runningBalance, accountName])
+            currDate += 1
+        i += 1
+
+
+    return balances
+
+
+
+
+
+
+
+def importMonarchTransactions():
+    importDir = getPath("import")
+    selection = selectImportFile(importDir)
+    runningBal = int(float(re.sub(",|\$","",input("What is the current account balance?: ")))*100)
+    transactions = {}
+
+    with open(str(selection), newline='') as impFile:
+        unsortedTable = csv.reader(impFile)
+
+        next(unsortedTable)
+
+        table = sorted(unsortedTable, key = lambda row: datetime.strptime(row[0], "%Y-%m-%d"), reverse=True)
+
+        for row in table:
+            if not row[0] == "Date":
+                date = (datetime.strptime(row[0], '%Y-%m-%d') - datetime(1899, 12, 30)).days
+                account = row[3]
+                amount = int(float(row[6])*100)
+                transactions[date] = [date, runningBal, account]
+
+                runningBal -= amount
+
+        transactions[date-1] = [date-1, runningBal, account]
+    
+    return getBalanceFromTransactions(transactions)
 
 def menu():
     usrInput = ""
@@ -138,7 +180,27 @@ e.Exit\n\
             break
 
         elif usrInput == "2":
+            while True:
+                usrInput = input(
+                    "Would you like to:\n\
+1.Convert Mint transactions to Monarch balances\n\
+2.Convert Mint trend account balances to Monarch balances\n\
+e.Exit\n\
+:"
+                )
+                if usrInput == "e":
+                    break
+                elif usrInput == "1":
+                    balances = importMintTransactions()
+                    break
+                elif usrInput == "2":
+                    balances = importMintBalances()
+                    break
+                else:
+                    print("Error: Please select one of the available options:")
             break
+        else:
+            print("Error: Please select one of the available options:")
     
     if balances != []:
         exportBalances(balances)
